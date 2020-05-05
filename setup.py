292fc6f625ec5efff3877ee41156ac1e7d0487e8
Mathew Bub <mathew.bub@gmail.com>
2020-05-05 19:11:13 -0400

    Add confinement package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='confinement',
+    version='0.1.dev',
+    packages=['confinement'],
+    url='https://github.com/mwbub/confinement',
+    author='Mathew Bub',
+    author_email='mathew.bub@gmail.com',
+    description='',
+    install_requires=['numpy', 'scipy']
+)
