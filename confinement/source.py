0ef51cfbf5a85de6f4b6ff02257716889aab450f
Mathew Bub <mathew.bub@gmail.com>
2020-05-13 22:22:13 -0400

    Add Meson

--- confinement/source.py
+++ confinement/source.py
@@ -38,7 +38,7 @@ class Source:
             raise ValueError("monodromy must be either 'above' or 'below'")
 
     def eom(self, field):
-        """Compute the field equation of motion due to this source.
+        """Compute the field equation of motion due to this Source.
 
         Parameters
         ----------
@@ -64,3 +64,53 @@ class Source:
                 laplacian[:, z_index, i] = -field_val
 
         return laplacian * self.monodromy
+
+
+class Meson:
+    """
+    Class representing an equally and oppositely charged quark-antiquark pair.
+    """
+
+    def __init__(self, z1, z2, charge):
+        """Initialize this Meson.
+
+        Parameters
+        ----------
+        z1 : float
+            Position of the quark along the y-axis. Should obey z1 < z2.
+        z2 : float
+            Position of the antiquark along the y-axis. Should obey z1 < z2.
+        charge : array_like
+            1D array giving the charge of the quark. For SU(N), this array
+            should have shape (N-1,).
+        """
+        self.z1 = z1
+        self.z2 = z2
+        self.charge = charge
+
+    def eom(self, field):
+        """Compute the field equation of motion due to this Meson.
+
+        Parameters
+        ----------
+        field : Field2D
+            The field on which to evaluate. For SU(N), this vector field should
+            have N-1 component scalar fields.
+
+        Returns
+        -------
+        laplacian : ndarray
+            Array giving the value of the Laplacian due to this Meson at each
+            point. Has the same shape as field.field.
+        """
+        field_val = self.charge * 2j * np.pi / field.gridsize ** 2
+        y_index = int(round(-field.zmin / field.gridsize))
+
+        laplacian = np.zeros_like(field.field)
+        for i in range(field.nz):
+            z = field.zmin + i * field.gridsize
+            if self.z1 <= z <= self.z2:
+                laplacian[:, i, y_index - 1] = field_val
+                laplacian[:, i, y_index] = -field_val
+
+        return laplacian
