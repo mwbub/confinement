97f0937d40f11d335818db2a1b3f555cc4eb459a
Mathew Bub <mathew.bub@gmail.com>
2020-05-22 19:36:31 -0400

    Add methods for relaxing mirror symmetric fields

--- confinement/relaxation.py
+++ confinement/relaxation.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import jit
+from .field import Field2D
 _ERASESTR = "                                                                  "
 
 
@@ -56,6 +57,7 @@ class RelaxationSolver:
             The relative error, defined as max(|f_new - f_old|) / max(|f_new|),
             of the final iteration.
         """
+        # Set the update method
         if method == 'jacobi':
             update = self._update_jacobi
         elif method == 'gauss':
@@ -63,6 +65,7 @@ class RelaxationSolver:
         else:
             raise ValueError("method must be 'jacobi' or 'gauss'")
 
+        # Update until the error is small or the max iteration count is reached
         i = 0
         error = np.inf
         for i in range(maxiter):
@@ -73,6 +76,7 @@ class RelaxationSolver:
             if error < tol:
                 break
 
+        # Print a newline before returning if using verbose mode
         if verbose:
             print()
 
@@ -144,6 +148,69 @@ class RelaxationSolver2D(RelaxationSolver):
         """
         super().__init__(field, func)
 
+    def symmetric_solve(self, tol=1e-9, maxiter=10000, omega=1., verbose=False):
+        """Solve the PDE assuming mirror symmetry about the centre of the grid.
+
+        This method solves the PDE on the left half of the field using the
+        Gauss-Seidel method, while imposing Neumann boundary conditions on the
+        centre column of the grid. The final field is then found by reflecting
+        the left half of the field onto the right half.
+
+        Parameters
+        ----------
+        tol : float
+            Relative error tolerance. The solver will consider the solution to
+            have converged once this threshold is reached.
+        maxiter : int
+            Maximum number of iterations until halting.
+        omega : float
+            The relaxation factor, used for solving with successive
+            over-relaxation or under-relaxation.
+        verbose : bool
+            If True, print the iteration number and current error after each
+            iteration.
+
+        Returns
+        -------
+        iterations : int
+            Number of iterations until the solution converged or maxiter was
+            reached.
+        error : float
+            The relative error, defined as max(|f_new - f_old|) / max(|f_new|),
+            of the final iteration.
+        """
+        # Create a new field to contain the left half of self.field
+        centre_index = (self.field.nz - 1) // 2
+        centre = self.field.z[centre_index]
+        left_field = Field2D(self.field.n, self.field.zmin, centre,
+                             self.field.ymin, self.field.ymax,
+                             self.field.gridsize)
+        left_field.field[:] = self.field.field[:, :centre_index + 1]
+
+        # Update until the error is small or the max iteration count is reached
+        i = 0
+        error = np.inf
+        for i in range(maxiter):
+            error = self._symmetric_gauss(left_field, omega)
+            if verbose:
+                outstr = "Iteration: {}\tError: {:.3g}".format(i + 1, error)
+                print("\r" + _ERASESTR + "\r" + outstr, end="\r")
+            if error < tol:
+                break
+
+        # Print a newline before returning if using verbose mode
+        if verbose:
+            print()
+
+        # Copy and reflect the left half of the field into the original field
+        self.field.field[:, :centre_index + 1] = left_field.field
+        if self.field.nz % 2 == 0:
+            self.field.field[:, centre_index + 1:] = left_field.field[:, ::-1]
+        else:
+            self.field.field[:, centre_index + 1:] = left_field.field[:, -2::-1]
+
+        return i + 1, error
+
     def _update_jacobi(self, omega):
         """Update the field using the Jacobi method of relaxation.
 
@@ -208,6 +275,35 @@ class RelaxationSolver2D(RelaxationSolver):
 
         # Update the field using compiled code
         _update_gauss2d(f, h, laplacian, omega)
+
+        # Compute the error
+        error = np.max(np.abs(f - f_old)) / np.max(np.abs(f))
+        return error
+
+    def _symmetric_gauss(self, field, omega):
+        """Update a mirror symmetric field using Gauss-Seidel relaxation.
+
+        Parameters
+        ----------
+        field : Field2D
+            The left half of the field to update. Here, field.field should be
+            equivalent to self.field.field[:, :(self.field.nz - 1) // 2 + 1].
+        omega : float
+            The relaxation factor, used for solving with successive
+            over-relaxation or under-relaxation.
+
+        Returns
+        -------
+        error : float
+            The current error, defined as max(|f_new - f_old|) / max(|f_new|).
+        """
+        f = field.field
+        h = field.gridsize
+        laplacian = self.func(field)
+        f_old = np.copy(f)
+
+        # Update the field using compiled code
+        _symmetric_gauss2d(f, h, laplacian, omega)
 
         # Compute the error
         error = np.max(np.abs(f - f_old)) / np.max(np.abs(f))
@@ -409,3 +505,41 @@ def _update_gauss1d(f, h, deriv, omega):
         residual = f[:, i - 1] + f[:, i + 1] - 2 * f[:, i] - h**2 * deriv[:, i]
         delta = residual * omega / 2
         f[:, i] += delta
+
+
+@jit(nopython=True)
+def _symmetric_gauss2d(f, h, laplacian, omega):
+    """Update a mirror-symmetric 2D field using Gauss-Seidel relaxation.
+
+    Parameters
+    ----------
+    f : ndarray
+        The left half of the field. Has shape (n, (nz - 1) // 2 + 1, ny), where
+        n is the number of components.
+    h : float
+        The gridsize.
+    laplacian : ndarray
+        Array of the laplacian at each point. Has the same shape as f.
+    omega : float
+        The relaxation parameter.
+
+    Returns
+    -------
+    None
+    """
+    # Update the interior points of the field
+    _update_gauss2d(f, h, laplacian, omega)
+
+    # Update the right boundary assuming mirror symmetry across the boundary
+    if f.shape[1] % 2 == 0:
+        for j in range(1, f.shape[2] - 1):
+            residual = (f[:, -2, j] + f[:, -1, j - 1] + f[:, -1, j + 1]
+                        - 3 * f[:, -1, j] - h**2 * laplacian[:, -1, j])
+            delta = residual * omega / 4
+            f[:, -1, j] += delta
+    else:
+        for j in range(1, f.shape[2] - 1):
+            residual = (2 * f[:, -2, j] + f[:, -1, j - 1] + f[:, -1, j + 1]
+                        - 4 * f[:, -1, j] - h**2 * laplacian[:, -1, j])
+            delta = residual * omega / 4
+            f[:, -1, j] += delta
