2ead7c0e28683e4fc9578bed64a7f5a053a3d936
Mathew Bub <mathew.bub@gmail.com>
2020-05-22 22:23:55 -0400

    Add parameter for a constant term in the Laplacian

--- confinement/relaxation.py
+++ confinement/relaxation.py
@@ -9,7 +9,7 @@ class RelaxationSolver:
     Abstract parent class of RelaxationSolver2D and RelaxationSolver1D.
     """
 
-    def __init__(self, field, func):
+    def __init__(self, field, func, constant=None):
         """Initialize this RelaxationSolver.
 
         Parameters
@@ -23,9 +23,22 @@ class RelaxationSolver:
             take as its argument the field, and return a complex-valued array
             of the same shape as field.field which gives the Laplacian at each
             point.
+        constant : array_like
+            A constant term to add to the Laplacian. This should be an array
+            with the same shape as field.field.
         """
         self.field = field
-        self.func = func
+
+        if func is not None:
+            self.func = func
+        else:
+            zeros = np.zeros_like(field.field)
+            self.func = lambda f: zeros
+
+        if constant is not None:
+            self.constant = np.array(constant)
+        else:
+            self.constant = 0
 
     def solve(self, method='gauss', tol=1e-9, maxiter=10000, omega=1.,
               verbose=False):
@@ -131,7 +144,7 @@ class RelaxationSolver2D(RelaxationSolver):
     using the relaxation method.
     """
 
-    def __init__(self, field, func):
+    def __init__(self, field, func, constant=None):
         """Initialize this RelaxationSolver2D.
 
         Parameters
@@ -145,8 +158,11 @@ class RelaxationSolver2D(RelaxationSolver):
             take as its argument the field, and return a complex-valued array
             of the same shape as field.field which gives the Laplacian at each
             point.
+        constant : array_like
+            A constant term to add to the Laplacian. This should be an array
+            with the same shape as field.field.
         """
-        super().__init__(field, func)
+        super().__init__(field, func, constant=constant)
 
     def symmetric_solve(self, tol=1e-9, maxiter=10000, omega=1., verbose=False):
         """Solve the PDE assuming mirror symmetry about the centre of the grid.
@@ -232,7 +248,7 @@ class RelaxationSolver2D(RelaxationSolver):
         # Store the field, grid size, and Laplacian in temporary variables
         f = self.field.field
         h = self.field.gridsize
-        laplacian = self.func(self.field)
+        laplacian = self.func(self.field) + self.constant
 
         # Compute the new values of the field using vectorized operations
         residual = (f[:, :-2, 1:-1] + f[:, 2:, 1:-1] + f[:, 1:-1, :-2]
@@ -270,8 +286,8 @@ class RelaxationSolver2D(RelaxationSolver):
         f_old = np.copy(self.field.field)
 
         # Update the field using compiled code
-        _update_gauss2d(self.field.field, self.field.gridsize,
-                        self.func(self.field), omega)
+        laplacian = self.func(self.field) + self.constant
+        _update_gauss2d(self.field.field, self.field.gridsize, laplacian, omega)
 
         # Compute the error
         delta = self.field.field - f_old
@@ -299,7 +315,8 @@ class RelaxationSolver2D(RelaxationSolver):
         f_old = np.copy(field.field)
 
         # Update the field using compiled code
-        _symmetric_gauss2d(field.field, field.gridsize, self.func(field), omega)
+        laplacian = self.func(field) + self.constant
+        _symmetric_gauss2d(field.field, field.gridsize, laplacian, omega)
 
         # Compute the error
         delta = field.field - f_old
@@ -313,7 +330,7 @@ class PoissonSolver2D(RelaxationSolver2D):
     vector field, using the relaxation method.
     """
 
-    def __init__(self, field, func):
+    def __init__(self, field, laplacian):
         """Initialize this PoissonSolver2D.
 
         Parameters
@@ -322,15 +339,11 @@ class PoissonSolver2D(RelaxationSolver2D):
             The vector field which defines the grid and where the solution will
             ultimately be stored. The solver assumes that the boundary
             conditions for the field have already been set.
-        func : callable(Field2D)
-            The function which defines the Laplacian of the field. This should
-            take as its argument the field, and return a complex-valued array
-            of the same shape as field.field which gives the Laplacian at each
-            point. It is assumed that func depends only on the y and z grid
-            coordinates, and not on the value of the field.
+        laplacian : array_like
+            The array which defines the Laplacian of the field at each point.
+            This must have the same shape as field.field.
         """
-        laplacian = func(field)
-        super().__init__(field, lambda f: laplacian)
+        super().__init__(field, None, constant=laplacian)
 
 
 class RelaxationSolver1D(RelaxationSolver):
@@ -340,7 +353,7 @@ class RelaxationSolver1D(RelaxationSolver):
     using the relaxation method.
     """
 
-    def __init__(self, field, func):
+    def __init__(self, field, func, constant=None):
         """Initialize this RelaxationSolver1D.
 
         Parameters
@@ -354,8 +367,11 @@ class RelaxationSolver1D(RelaxationSolver):
             should take as its argument the field, and return a complex-valued
             array of the same shape as field.field which gives the second
             derivative at each point.
+        constant : array_like
+            A constant term to add to the second derivative. This should be an
+            array with the same shape as field.field.
         """
-        super().__init__(field, func)
+        super().__init__(field, func, constant=constant)
 
     def _update_jacobi(self, omega):
         """Update the field using the Jacobi method of relaxation.
@@ -378,7 +394,7 @@ class RelaxationSolver1D(RelaxationSolver):
         # Store the field, grid size, and 2nd derivative in temporary variables
         f = self.field.field
         h = self.field.gridsize
-        deriv = self.func(self.field)
+        deriv = self.func(self.field) + self.constant
 
         # Compute the new values of the field using vectorized operations
         residual = f[:, :-2] + f[:, 2:] - h**2 * deriv[:, 1:-1] - 2 * f[:, 1:-1]
@@ -414,8 +430,8 @@ class RelaxationSolver1D(RelaxationSolver):
         f_old = np.copy(self.field.field)
 
         # Update the field using compiled code
-        _update_gauss1d(self.field.field, self.field.gridsize,
-                        self.func(self.field), omega)
+        deriv = self.func(self.field) + self.constant
+        _update_gauss1d(self.field.field, self.field.gridsize, deriv, omega)
 
         # Compute the error
         delta = self.field.field - f_old
@@ -429,7 +445,7 @@ class PoissonSolver1D(RelaxationSolver1D):
     vector field, using the relaxation method.
     """
 
-    def __init__(self, field, func):
+    def __init__(self, field, deriv):
         """Initialize this PoissonSolver1D.
 
         Parameters
@@ -438,15 +454,11 @@ class PoissonSolver1D(RelaxationSolver1D):
             The vector field which defines the grid and where the solution will
             ultimately be stored. The solver assumes that the boundary
             conditions for the field have already been set.
-        func : callable(Field1D)
-            The function which defines the second derivative of the field. This
-            should take as its argument the field, and return a complex-valued
-            array of the same shape as field.field which gives the second
-            derivative at each point. It is assumed that func depends only on
-            the grid coordinates, and not on the value of the field.
+        deriv : array_like
+            The array which defines the second derivative of the field at each
+            point. This must have the same shape as field.field.
         """
-        deriv = func(field)
-        super().__init__(field, lambda f: deriv)
+        super().__init__(field, None, constant=deriv)
 
 
 @jit(nopython=True)
